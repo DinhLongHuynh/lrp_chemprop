b62ef550abcca56b5a403ceebbed6de925f97cab
Dinh Long Huynh <151563450+DinhLongHuynh@users.noreply.github.com>
2025-03-11 11:43:05 +0100

    Update descriptor Correctness_Analyzer.py

--- lrp_chemprop/Correctness_Analyzer.py
+++ lrp_chemprop/Correctness_Analyzer.py
@@ -3,12 +3,32 @@ from scipy.stats import kendalltau
 import torch
 
 class Correctness_Analyzer():
+    '''A class to perform correctness analysis given relevance score and ground truth of each atom.
+
+    Parameters:
+    ---------
+    relevance_score (dict): a dictionary with keys of 'Compound_id' and values are lists of lrp relevance scores of each atom in the respective compounds.
+    ground_truth (dict): a dictionary with keys of 'Compound_id' and values are lists of ground-truth scores of each atom in the respective compounds.
+    '''
+    
     def __init__(self, relevance_score, ground_truth):
         self.relevance_score = relevance_score
         self.ground_truth = ground_truth
         self.num_compound = len(ground_truth)
+
     
     def quantile_analyzer(self, quantile_width):
+        '''Perform correctness analysis by considering the most important part of the molecule. The 'important part' was defined by the quantile threshold of relevance scores.
+        
+        Parameters:
+        ---------
+        quantile_width (float): the granularity of calculation. The quantile range is always from 0 to 1, with a width defined by quantile_width.
+
+        Returns: 
+        ----------
+        tau_avgs (numpy array): an array that contains the average kendall tau's correlation at each quantile threshold.
+        '''
+        
         tau_avgs = []
         for i in np.arange(0,1+quantile_width,quantile_width):
             ground_truth_copy = {k: v.clone() for k, v in self.ground_truth.items()}
@@ -45,16 +65,39 @@ class Correctness_Analyzer():
         return tau_avgs
     
 
-
     def get_minimum_value(self,tensor,order):
+        '''Determine the 'order'_th minimum value in a 1D tensor.
+        
+        Parameters:
+        ---------
+        tensor (torch Tensor): 1D tensor.
+        order (int): the rank of the minimum value to retrieve.
+
+        Returns: 
+        ----------
+        minimum (float): the 'order'_th minimum value in the tensor.
+        '''
+        
         tensor_sorted = tensor.sort().values
         
         if order > tensor.shape[-1]:
             return tensor.max().item()
         minimum = tensor_sorted[order-1]
         return minimum
+
     
     def atom_analyzer(self,num_atoms):
+        '''Perform correctness analysis by considering the most important part of the molecule. The 'important part' was defined by the most important atoms according to relevance scores.
+        
+        Parameters:
+        ---------
+        num_atoms (int): Number of most important atoms to consider.
+
+        Returns: 
+        ----------
+        tau_avgs (numpy array): an array that contains the average kendall tau's correlation at each num_atoms threshold.
+        '''
+        
         tau_avgs = []
         for num_atom in np.arange(1,num_atoms):
             ground_truth_copy = {k: v.clone() for k, v in self.ground_truth.items()}
