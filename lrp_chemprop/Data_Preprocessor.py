b80fdd8e609e33534ba9a529b36538a178535f53
Dinh Long Huynh <151563450+DinhLongHuynh@users.noreply.github.com>
2025-03-11 11:05:14 +0100

    Update descriptor Data_Preprocessor.py

--- lrp_chemprop/Data_Preprocessor.py
+++ lrp_chemprop/Data_Preprocessor.py
@@ -9,17 +9,24 @@ from chemprop import data, featurizers
 
 
 class Data_Preprocessor:
+    '''A class to prepare Chemprop dataset from Pandas dataframe.'''
+    
     def __init__(self):
         pass
+
     
     def is_hbd(self,atom):
-        """
-        Check if an atom is a Hydrogen Bond Donor (HBD). An atom is considered an HBD if it's N or O with at least one hydrogen.
-        Args:
-            atom: RDKit atom object
+        '''Check if an atom is a Hydrogen Bond Donor (HBD). An atom is considered an HBD if it's N or O with at least one hydrogen.
+        
+        Parameters:
+        ----------
+        atom: RDKit atom object.
+            
         Returns:
-            bool: True if atom is HBD, False otherwise
-        """
+        ----------
+        bool: True if atom is HBD, False otherwise.
+        '''
+        
         if atom.GetAtomicNum() not in [7, 8]:  # 7 for N, 8 for O
             return False
         
@@ -27,13 +34,17 @@ class Data_Preprocessor:
         return n_hydrogens > 0
 
     def is_hba(self,atom):
-        """
-        Check if an atom is a Hydrogen Bond Acceptor (HBA). An atom is considered an HBA if it's N or O with a lone pair electron
-        Args:
-            atom: RDKit atom object
+        '''Check if an atom is a Hydrogen Bond Acceptor (HBA). An atom is considered an HBA if it's N or O with a lone pair electron
+        
+        Parameters:
+        ----------
+        atom: RDKit atom object.
+            
         Returns:
-            bool: True if atom is HBA, False otherwise
-        """
+        ----------
+        bool: True if atom is HBD, False otherwise.
+        '''
+        
         atomic_num = atom.GetAtomicNum()
         if atomic_num not in [7, 8]: 
             return False
@@ -45,8 +56,14 @@ class Data_Preprocessor:
             return valence <= 2  
     
 
-    # Prepare dataset without additional HBD/HBA features
     def dataset_generator_no_HBD_HBA(self):
+        '''Prepare chemprop dataset without additional HBD/HBA feature.
+    
+        Returns:
+        ----------
+        dataset (Chemprop dataset): Chemprop dataset.
+        '''
+        
         def datapoint_generator(df,smiles,y,addH):
             smis = df.loc[:,smiles].values
             ys = df.loc[:,[y]].values
@@ -59,8 +76,14 @@ class Data_Preprocessor:
         return dataset
     
 
-    # Prepare dataset with additional HBD/HBA features
     def dataset_generator_HBD_HBA(self):
+        '''Prepare chemprop dataset with additional HBD/HBA feature.
+    
+        Returns:
+        ----------
+        dataset (Chemprop dataset): Chemprop dataset.
+        '''
+        
         def datapoint_generator(df,smiles,y,addH):
             smis = df.loc[:,smiles].values
             ys = df.loc[:,[y]].values
@@ -90,9 +113,25 @@ class Data_Preprocessor:
         dataset = data.MoleculeDataset(datapoints, featurizer=self.featurizer)
         return dataset
     
-    # Generate chemprop dataset
+
     def generate(self, df, smiles_column = 'smiles', target_column='docking_score', addH=False, HB = False,
                  featurizer = featurizers.SimpleMoleculeMolGraphFeaturizer()):
+        '''Generate chemprop dataset according to a given configuration
+
+        Parameters:
+        ----------
+        df (Pandas DataFrame): a data frame that contains SMILES code of compounds.
+        smiles_column (str): a string that indicates SMILES column in the data frame.
+        target_column (str): a string that indicates the target column (i.e. docking_scores, solubility) in the data frame.
+        addH (boolean): to incorporate explicit hydrogen atoms into a molecular graph.
+        HB (boolean): to incorporate additional HBD/HBA features for each atom in BatchMolGraph.
+        featurizer (Chemprop Featurizer): a Featurizer from Chemprop to encode features for atoms, bonds, and molecules.
+    
+        Returns:
+        ----------
+        dataset (Chemprop dataset): Chemprop dataset
+        '''
+                     
         self.df = df
         self.smiles_column = smiles_column
         self.target_column = target_column
