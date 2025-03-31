5fabdf2d20d064a5abba7931190e5dbef2969d02
Dinh Long Huynh <151563450+DinhLongHuynh@users.noreply.github.com>
2025-03-31 12:00:23 +0200

    script for process glide dock results

---
+++ lrp_chemprop/utils/glide_docking_processing.py
@@ -0,0 +1,22 @@
+import os
+import pandas as pd
+
+
+folder_name = 'glide-dock_SP_'
+job_list = [2,3,4,9]
+
+def glide_result(folder_name=folder_name,job_list=job_list):
+    for job in job_list:
+        file_name = folder_name+str(job)+'/'+folder_name+str(job)+'.csv'
+        df = pd.read_csv(file_name)
+        df_filter = df[['title','r_i_glide_gscore']]
+        df_filter.columns = ['id','docking_score']
+        df_sort = df_filter.sort_values(['id','docking_score'], ascending=True)
+        df_unique = df_sort.drop_duplicates(subset=['id'],keep='first')
+
+        df_unique.to_csv(folder_name+str(job)+'_processed.csv', index=False)
+
+    return df_unique
+
+if __name__ == "__main__":
+    glide_result()
