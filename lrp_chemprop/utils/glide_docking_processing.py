8d04a9442b3114e26997ee3337939630caca5531
Dinh Long Huynh <151563450+DinhLongHuynh@users.noreply.github.com>
2025-04-10 08:40:12 +0200

    use docking_score instead of glide_score

--- lrp_chemprop/utils/glide_docking_processing.py
+++ lrp_chemprop/utils/glide_docking_processing.py
@@ -1,15 +1,16 @@
 import os
 import pandas as pd
 
+
 folder_name = 'glide-dock_SP_'
 job_list = input('Insert Job List:')
 job_list = job_list.split(", ")
 
-def glide_result(folder_name, job_list):
+def glide_result(folder_name,job_list):
     for job in job_list:
         file_name = folder_name+str(job)+'/'+folder_name+str(job)+'.csv'
         df = pd.read_csv(file_name)
-        df_filter = df[['title','r_i_glide_gscore']]
+        df_filter = df[['title','r_i_docking_gscore']]
         df_filter.columns = ['id','docking_score']
         df_sort = df_filter.sort_values(['id','docking_score'], ascending=True)
         df_unique = df_sort.drop_duplicates(subset=['id'],keep='first')
@@ -17,7 +18,9 @@ def glide_result(folder_name, job_list):
 
         df_unique.to_csv(folder_name+str(job)+'_processed.csv', index=False)
         
+
     return df_unique
 
+
 if __name__ == "__main__":
-    glide_result(folder_name=folder_name,job_list=job_list)
+    glide_result()
