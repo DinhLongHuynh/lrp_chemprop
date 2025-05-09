5863c85693cd281b4555ef6b5a06dbc0166d77f0
Dinh Long Huynh <151563450+DinhLongHuynh@users.noreply.github.com>
2025-05-09 22:38:00 +0200

    Update standardize_smiles.py

--- lrp_chemprop/utils/standardize_smiles.py
+++ lrp_chemprop/utils/standardize_smiles.py
@@ -1,28 +1,15 @@
-import pandas as pd
 from rdkit import Chem
-from tqdm import tqdm
+from molvs import Standardizer
 
-tqdm.pandas()
-def standardize_smiles(smiles,desalt,remove_stereo):
-    mol = Chem.MolFromSmiles(smiles)
-    if mol is None:
-        return None
-    try:
-        if desalt:
-            frags = Chem.GetMolFrags(mol, asMols=True)
-            mol = max(frags, key=lambda m: m.GetNumAtoms())
-        if remove_stereo:
-            Chem.RemoveStereochemistry(mol)
-        
-        Chem.SanitizeMol(mol)
-        return Chem.MolToSmiles(mol, canonical=True)
-    
-    except Exception as e:
-        print(f"Error in standardizing {smiles}: {str(e)}")
-        return mol 
+class SMILES_standardize:
+    def __init__(self, smiles):
+        self.smiles = smiles
+        self.mol = Chem.MolFromSmiles(smiles)
+        Chem.SanitizeMol(self.mol)
 
-
-def standardize_smiles_df(df,smile_columns = 'smiles',desalt=True,remove_stereo=True):
-    df['smiles_standard'] = df[smile_columns].progress_apply(lambda smiles: standardize_smiles(smiles,desalt=desalt,remove_stereo=remove_stereo))
-    return df
-
+    def standardizer(self):
+        self.mol = Standardizer().fragment_parent(self.mol, skip_standardize=False)
+        self.mol = Standardizer().isotope_parent(self.mol, skip_standardize=False)
+        self.mol = Standardizer().charge_parent(self.mol, skip_standardize=False)
+        #self.mol = Standardizer().stereo_parent(self.mol, skip_standardize=False)
+        self.mol = Standardizer().tautomer_parent(self.mol, skip_standardize=False)
