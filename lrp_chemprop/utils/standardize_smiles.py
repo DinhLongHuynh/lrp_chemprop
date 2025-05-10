dc9b9c7fdc91a3df10bfe9c63a555c284048fd3e
Dinh Long Huynh <151563450+DinhLongHuynh@users.noreply.github.com>
2025-05-10 21:17:59 +0200

    Update standardize_smiles.py

--- lrp_chemprop/utils/standardize_smiles.py
+++ lrp_chemprop/utils/standardize_smiles.py
@@ -8,8 +8,12 @@ class SMILES_standardize:
         Chem.SanitizeMol(self.mol)
 
     def standardizer(self):
-        self.mol = Standardizer().fragment_parent(self.mol, skip_standardize=False)
-        self.mol = Standardizer().isotope_parent(self.mol, skip_standardize=False)
-        self.mol = Standardizer().charge_parent(self.mol, skip_standardize=False)
-        #self.mol = Standardizer().stereo_parent(self.mol, skip_standardize=False)
-        self.mol = Standardizer().tautomer_parent(self.mol, skip_standardize=False)
+        try:
+            self.std_mol = Standardizer().fragment_parent(self.mol, skip_standardize=False)
+            self.std_mol  = Standardizer().isotope_parent(self.std_mol, skip_standardize=False)
+            self.std_mol  = Standardizer().charge_parent(self.std_mol, skip_standardize=False)
+            #self.std_mol = Standardizer().stereo_parent(self.std_mol, skip_standardize=False)
+            self.std_mol = Standardizer().tautomer_parent(self.std_mol, skip_standardize=False)
+            return Chem.MolToSmiles(self.std_mol, canonical = True)
+        except Exception:
+            return self.smiles
